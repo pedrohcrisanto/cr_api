bf921dc2cb99587d0be33a02045a6afc02638bc5
Pedro Crisanto <pedrohcrisanto@gmail.com>
2020-06-19 15:51:35 -0300

    Ajust Serializer

--- app/serializers/cart_item_serializer.rb
+++ app/serializers/cart_item_serializer.rb
@@ -1,7 +1,18 @@
 class CartItemSerializer < ActiveModel::Serializer
-  attributes :id, :medicine_id, :quantity, :cart_id, :total
+  attributes :id, :client, :medicine_name, :quantity, :value, :total
   belongs_to :cart
   belongs_to :medicine
+  def value
+    Medicine.find(self.object.medicine_id).value
+  end
+
+  def medicine_name
+    Medicine.find(self.object.medicine_id).name
+  end
+
+  def client
+    Customer.find(self.object.cart.customer_id).name
+  end
 
   def total
     medicine = Medicine.find(self.object.medicine_id)
