64df3c7bed0e6974bc37b92d723adf438edcb074
Pedro Crisanto <pedrohcrisanto@gmail.com>
2020-06-21 16:00:42 -0300

    Discount function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,4 +9,20 @@ class Cart < ApplicationRecord
     end
     total
   end
+
+  def discount
+      case self.cart_items.ids.uniq.size
+      when 1
+        total_discount = self.total
+      when 2
+        total_discount = self.total - (self.total * 0.05)
+      when 3
+        total_discount = self.total - (self.total * 0.1)
+      when 4
+        total_discount = self.total - (self.total * 0.2)
+      else
+        total_discount = self.total - (self.total * 0.25)
+      end
+      total_discount
+  end
 end
--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,5 +1,5 @@
 class CartSerializer < ActiveModel::Serializer
-  attributes :id, :customer_name, :total
+  attributes :id, :customer_name, :total, :total_discount
 
   belongs_to :customer
   has_many :cart_items
@@ -7,7 +7,9 @@ class CartSerializer < ActiveModel::Serializer
   def customer_name
     Customer.find(self.object.customer_id).name.split.each{|i| i.capitalize!}.join(' ')
   end
-
+  def total_discount
+    self.object.discount
+  end
   def total
     self.object.total
   end
